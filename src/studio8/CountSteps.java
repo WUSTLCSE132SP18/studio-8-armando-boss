47fd0a326fa348b9e158e1e87b48e414b6318b4c
Armando Q <qmando98@gmail.com>
2018-03-19 15:15:27 -0500

    progress

--- src/studio8/CountSteps.java
+++ src/studio8/CountSteps.java
@@ -1,7 +1,36 @@
 package studio8;
 
+import jssc.SerialPort;
+import jssc.SerialPortException;
+
 // TODO: Develop an algorithm to count steps in accelerometer data
 //    Major steeps:
 //       1. Create a class and main method.
 //       2. Using a Scanner and File object, read data from your .csv file.
 //       3. Develop and test algorithms to count the "peaks" in the data.
+
+public class CountSteps{
+	
+	SerialPort port;
+
+	private boolean debug;  // Indicator of "debugging mode"
+	
+	// This function can be called to enable or disable "debugging mode"
+	void setDebug(boolean mode) {
+		debug = mode;
+	}	
+	
+
+	// Constructor for the SerialComm class
+	public SerialComm(String name) throws SerialPortException {
+		port = new SerialPort(name);		
+		port.openPort();
+		port.setParams(SerialPort.BAUDRATE_9600,
+			SerialPort.DATABITS_8,
+			SerialPort.STOPBITS_1,
+			SerialPort.PARITY_NONE);
+		
+		debug = false; // Default is to NOT be in debug mode
+	}
+	
+}
